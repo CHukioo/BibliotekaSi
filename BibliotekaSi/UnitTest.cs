e5382aebba0b838b3314a522a9c39c832ee3ccd5
CHukioo <markocurlinoski.uie@gmail.com>
2016-11-17 12:16:37 +0100

    Dali Paga CI build

---
+++ BibliotekaSi/UnitTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekaSi
+{
+    [TestFixture]
+    class UnitTest
+    {
+        [TestCase]
+        public void Add()
+        {
+            PrvTest test = new PrvTest();
+            Assert.AreEqual(1, test.Soberi(20, 11));
+        }
+    }
+}
---
+++ BibliotekaSi/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BibliotekaSi
+{
+    [TestFixture]
+    class UnitTests
+    {
+        [TestCase]
+        public void Soberi()
+        {
+            PrvTest test = new PrvTest();
+            Assert.AreEqual(35, test.Soberi(20, 11));
+        }
+    }
+}
